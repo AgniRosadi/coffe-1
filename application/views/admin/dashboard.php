25d98a65288478f759ffe8eb30b23516044f508d
Lan-Devlop <landev309@gmail.com>
2021-04-25 12:50:28 +0700

    perubahan pada pengelola caffe

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,102 +1,151 @@
- 
  <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
+     <div class="container-fluid">
+         <div class="row mb-2">
+         </div><!-- /.row -->
+     </div><!-- /.container-fluid -->
+ </div>
 
  <section class="content">
- 	<div class="container-fluid">
- 		<!-- Info boxes -->
- 		<div class="row">
- 			<div class="col-12 col-sm-6 col-md-3">
- 				<div class="info-box">
- 					<span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+     <div class="container-fluid">
+         <!-- Info boxes -->
+         <div class="row">
+             <div class="col-lg-4 col-6">
+                 <!-- small card -->
+                 <div class="small-box bg-info">
+                     <div class="inner">
+                         <h3>150</h3>
 
- 					<div class="info-box-content">
- 						<span class="info-box-text">SANGGAR</span>
- 						<span class="info-box-number">
- 							<small>sanggar</small>
- 						</span>
- 					</div>
- 					<!-- /.info-box-content -->
- 				</div>
- 				<!-- /.info-box -->
- 			</div>
- 			<!-- /.col -->
- 			<div class="col-12 col-sm-6 col-md-3">
- 				<div class="info-box mb-3">
- 					<span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+                         <p>Jumlah Pengajuan</p>
+                     </div>
+                     <div class="icon">
+                         <i class="fas fa-tasks"></i>
+                     </div>
+                     <a href="#" class="small-box-footer">
+                         More info <i class="fas fa-arrow-circle-right"></i>
+                     </a>
+                 </div>
+             </div>
+             <!-- ./col -->
+             <div class="col-lg-4 col-6">
+                 <!-- small card -->
+                 <div class="small-box bg-info">
+                     <div class="inner">
+                         <h3>53<sup style="font-size: 20px">%</sup></h3>
 
- 					<div class="info-box-content">
- 						<span class="info-box-text">Pengajuan</span>
- 						<span class="info-box-number"></span>
- 					</div>
- 					<!-- /.info-box-content -->
- 				</div>
- 				<!-- /.info-box -->
- 			</div>
- 			<!-- /.col -->
+                         <p>Coffe Terdaftar</p>
+                     </div>
+                     <div class="icon">
+                         <i class="fas fa-store"></i>
+                     </div>
+                     <a href="#" class="small-box-footer">
+                         More info <i class="fas fa-arrow-circle-right"></i>
+                     </a>
+                 </div>
+             </div>
+             <!-- ./col -->
+             <div class="col-lg-4 col-6">
+                 <!-- small card -->
+                 <div class="small-box bg-info">
+                     <div class="inner">
+                         <h3>44</h3>
 
- 			<!-- fix for small devices only -->
- 			<div class="clearfix hidden-md-up"></div>
+                         <p>Jumlah Penolakan</p>
+                     </div>
+                     <div class="icon">
+                         <i class="far fa-window-close"></i>
+                     </div>
+                     <a href="#" class="small-box-footer">
+                         More info <i class="fas fa-arrow-circle-right"></i>
+                     </a>
+                 </div>
+             </div>
+             <div class="col-12 col-sm-6 col-md-3">
+                 <div class="info-box">
+                     <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
- 			<div class="col-12 col-sm-6 col-md-3">
- 				<div class="info-box mb-3">
- 					<span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+                     <div class="info-box-content">
+                         <span class="info-box-text">SANGGAR</span>
+                         <span class="info-box-number">
+                             <small>sanggar</small>
+                         </span>
+                     </div>
+                     <!-- /.info-box-content -->
+                 </div>
+                 <!-- /.info-box -->
+             </div>
+             <!-- /.col -->
+             <div class="col-12 col-sm-6 col-md-3">
+                 <div class="info-box mb-3">
+                     <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
- 					<div class="info-box-content">
- 						<span class="info-box-text">Kategori</span>
- 						<span class="info-box-number">
- 							<small>kategori</small>
- 						</span>
+                     <div class="info-box-content">
+                         <span class="info-box-text">Pengajuan</span>
+                         <span class="info-box-number"></span>
+                     </div>
+                     <!-- /.info-box-content -->
+                 </div>
+                 <!-- /.info-box -->
+             </div>
+             <!-- /.col -->
 
- 					</div>
- 					<!-- /.info-box-content -->
- 				</div>
- 				<!-- /.info-box -->
- 			</div>
- 			<!-- /.col -->
- 			<div class="col-12 col-sm-6 col-md-3">
- 				<div class="info-box mb-3">
- 					<span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+             <!-- fix for small devices only -->
+             <div class="clearfix hidden-md-up"></div>
 
- 					<div class="info-box-content">
- 						<span class="info-box-text">Jumlah Postingan</span>
- 						<span class="info-box-number"></span>
- 					</div>
- 					<!-- /.info-box-content -->
- 				</div>
- 				<!-- /.info-box -->
- 			</div>
+             <div class="col-12 col-sm-6 col-md-3">
+                 <div class="info-box mb-3">
+                     <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
- 			<div class="col-12 col-sm-6 col-md-3">
- 				<div class="info-box mb-3">
- 					<span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+                     <div class="info-box-content">
+                         <span class="info-box-text">Kategori</span>
+                         <span class="info-box-number">
+                             <small>kategori</small>
+                         </span>
 
- 					<div class="info-box-content">
- 						<span class="info-box-text">Jumlah Kecamatan</span>
- 						<span class="info-box-number"></span>
- 					</div>
- 					<!-- /.info-box-content -->
- 				</div>
- 				<!-- /.info-box -->
- 			</div>
+                     </div>
+                     <!-- /.info-box-content -->
+                 </div>
+                 <!-- /.info-box -->
+             </div>
+             <!-- /.col -->
+             <div class="col-12 col-sm-6 col-md-3">
+                 <div class="info-box mb-3">
+                     <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
- 			<div class="col-12 col-sm-6 col-md-3">
- 				<div class="info-box mb-3">
- 					<span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+                     <div class="info-box-content">
+                         <span class="info-box-text">Jumlah Postingan</span>
+                         <span class="info-box-number"></span>
+                     </div>
+                     <!-- /.info-box-content -->
+                 </div>
+                 <!-- /.info-box -->
+             </div>
 
- 					<div class="info-box-content">
- 						<span class="info-box-text">Jumlah Kelurahan</span>
- 						<span class="info-box-number"></span>
- 					</div>
- 					<!-- /.info-box-content -->
- 				</div>
- 				<!-- /.info-box -->
- 			</div>
- 			<!-- /.col -->
- 		</div>
- 	</div>
+             <div class="col-12 col-sm-6 col-md-3">
+                 <div class="info-box mb-3">
+                     <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+
+                     <div class="info-box-content">
+                         <span class="info-box-text">Jumlah Kecamatan</span>
+                         <span class="info-box-number"></span>
+                     </div>
+                     <!-- /.info-box-content -->
+                 </div>
+                 <!-- /.info-box -->
+             </div>
+
+             <div class="col-12 col-sm-6 col-md-3">
+                 <div class="info-box mb-3">
+                     <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+
+                     <div class="info-box-content">
+                         <span class="info-box-text">Jumlah Kelurahan</span>
+                         <span class="info-box-number"></span>
+                     </div>
+                     <!-- /.info-box-content -->
+                 </div>
+                 <!-- /.info-box -->
+             </div>
+             <!-- /.col -->
+         </div>
+     </div>
  </section>
